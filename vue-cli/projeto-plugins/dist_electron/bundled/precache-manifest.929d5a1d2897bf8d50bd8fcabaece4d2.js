80004e56d197b0ad2553c2803b7f72f37bfd4f89
Natanbcr <natanbcr@primeinformatica.com.br>
2019-04-18 14:58:44 -0300

    as pastas fudidas

---
+++ vue-cli/projeto-plugins/dist_electron/bundled/precache-manifest.929d5a1d2897bf8d50bd8fcabaece4d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "app://./robots.txt"
+  },
+  {
+    "revision": "bf6869f9c18aba7f1bb6",
+    "url": "app://./js/chunk-vendors.1f400729.js"
+  },
+  {
+    "revision": "c08da70c54ade2cadb40",
+    "url": "app://./js/app.3f9eddb9.js"
+  },
+  {
+    "revision": "de1f713075e31f01e6a7",
+    "url": "app://./js/about.32b639b0.js"
+  },
+  {
+    "revision": "d7b120c2c790d2a7eaf1b34d43d9c492",
+    "url": "app://./index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "app://./img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "bf6869f9c18aba7f1bb6",
+    "url": "app://./css/chunk-vendors.86f3e0f0.css"
+  }
+];
