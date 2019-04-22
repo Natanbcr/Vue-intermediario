4a4cd659cf817f299e086e30526abd8f1433681f
Natanbcr <natanbcr@primeinformatica.com.br>
2019-04-22 14:09:05 -0300

    componentes completo

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="citacao">
+       <slot name="autor"></slot>
+       <slot></slot>
+
+       <div class="fonte">
+           <slot name="fonte"></slot>
+       </div>
+       
+    </div>
+</template>
+
+<script>
+export default {
+   created() {
+       console.log('created')
+   },
+   destroyed() {
+       console.log('destroyed')
+   },
+   activated() {
+       console.log('activated')
+   },
+   deactivated() {
+       console.log('deactivated')
+   }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+   
+   
+   h1 {
+       /*faz com que essa classe seja lida*/
+       color: red!important;
+   }
+
+   .fonte {
+       float: right;
+   }
+
+</style>
---
+++ componentes-avancados/comp-avancado-desafio/src/components/Verde.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="caixa verde">
+
+  <slot name="conteudo"></slot>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
