88c425225d2b7c34fca76ca7220519c47734e492
Natanbcr <natanbcr@primeinformatica.com.br>
2019-04-22 16:02:47 -0300

    Projeto ncpl

--- projeto-02-tarefas/tarefas/src/App.vue
+++ projeto-02-tarefas/tarefas/src/App.vue
@@ -1,19 +1,30 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<TaskGrid :tasks="tasks" />
 	</div>
 </template>
 
 <script>
-export default {
+import TaskGrid from './components/TaskGrid.vue'
 
+export default {
+	components: { TaskGrid },
+     data() {
+		 return {
+			 tasks: [
+				 {name : 'Lavar a louça', pending: false},
+				 {name : 'Comprar blusa', pending: true},
+			 ]
+		 }
+	 }
 }
 </script>
 
 <style>
 	body {
 		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+		background: linear-gradient(to right, rgb(22, 34, 42), rgb(50, 121, 156));
 		color: #FFF;
 	}
 
