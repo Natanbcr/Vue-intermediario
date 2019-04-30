be701debdbc61835743a24fcac25b9c9a5c9ec10
Natanbcr <natanbcr@primeinformatica.com.br>
2019-04-30 14:35:53 -0300

    filtros e mixins complt

--- animacoes/animacoes-exercicios/src/main.js
+++ animacoes/animacoes-exercicios/src/main.js
@@ -1,11 +1,10 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
-Vue.filter('contarPalavras', function(valor){
-	return valor.split(' ').map(p => `${p} (${p.length})`).join('')
-})
 new Vue({
 	render: h => h(App),
 }).$mount('#app')
