4ec77b755825085701d98cf22340d9c49e351777
Natanbcr <natanbcr@primeinformatica.com.br>
2019-05-06 10:51:19 -0300

    transition-group

---
+++ animacoes/animacoes-exercicios/src/AlertaAdvertencia.vue
@@ -0,0 +1,10 @@
+<template>
+  <b-alert variant="warning" show style="color: red;">danger danger danger!</b-alert>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -1,24 +1,100 @@
 <template>
   <div id="app" class="container-fluid">
     <h1>Animações</h1>
+    <!-- <hr>
+    <b-button variant="primary" class="mb-4" @click="exibir = !exibir">Mostrar Mensagem</b-button>
     <hr>
-    <b-button variant="primary" class="mb-4"
-	@click="exibir = !exibir">Mostrar Mensagem</b-button>
+    <b-select v-model="tipoAnimacao" class="mb-4">
+      <option value="fade">Fade</option>
+      <option value="slide">Slide</option>
+    </b-select>
 
-	<transition name="fade">
-<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
-	</transition>
-	
+    <transition :name="tipoAnimacao" mode="out-in">
+      <b-alert variant="info" show v-if="exibir" key="info">{{ msg }}</b-alert>
+      <b-alert variant="warning" show v-else key="warn">{{ msg }}</b-alert>
+    </transition>
+    <hr>
+    <button @click="exibir2 = !exibir2">Alternar</button>
+    <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+    >
+      <div v-if="exibir2" class="caixa"></div>
+    </transition> -->
+
+    <!-- <hr>
+    <div class="mb-4">
+      <b-button variant="primary" class="mr-2" @click="componenteSelecionado = 'AlertaInfo'">Info</b-button>
+      <b-button variant="danger" @click="componenteSelecionado = 'AlertaAdvertencia'">Advertência</b-button>
+    </div>
+    <transition name="fade" mode="out-in">
+      <component :is="componenteSelecionado"></component>
+    </transition> -->
+    <hr>
+    <b-button variant="primary" @click="adicionarAluno" class="mb-4"> Adicionar Aluno </b-button>
+    <transition-group name="slide" tag="div">
+    <b-list-group v-for="(aluno, i) in alunos" :key="aluno">
+      <b-list-group-item @click="removerAluno(i)" >{{ aluno }}</b-list-group-item>
+    </b-list-group>
+    </transition-group>
   </div>
 </template>
 
 <script>
+//import AlertaAdvertencia from "./AlertaAdvertencia.vue";
+//import AlertaInfo from "./AlertaInfo.vue";
+
 export default {
+  //components: { AlertaAdvertencia, AlertaInfo },
   data() {
     return {
+      alunos: ['Natan', 'Gabriel', 'Lucax', 'Filipe'],
       msg: "Uma mensagem de informação para o usuário!",
-      exibir: false
+      exibir: false,
+      exibir2: true,
+      tipoAnimacao: "fade",
+      larguraBase: 0,
+      componenteSelecionado: "AlertaInfo"
     };
+  },
+  methods: {
+    adicionarAluno() {
+       const s = Math.random().toString(36).substring(2)
+       this.alunos.push(s)
+    },
+    removerAluno(indice) {
+      this.alunos.splice(indice, 1)
+    },
+    animar(el, done, negativo) {
+      let rodada = 1;
+      const temporizador = setInterval(() => {
+        const novaLargura =
+          this.larguraBase + (negativo ? -rodada * 10 : rodada * 10);
+        el.style.width = `${novaLargura}px`;
+        rodada++;
+        if (rodada > 30) {
+          clearInterval(temporizador);
+          done();
+        }
+      }, 20);
+    },
+    beforeEnter(el) {
+      this.larguraBase = 0;
+      el.style.width = `${this.larguraBase}px`;
+    },
+    enter(el, done) {
+      this.animar(el, done, false);
+    },
+
+    beforeLeave(el) {
+      this.larguraBase = 300;
+      el.style.width = `${this.larguraBase}px`;
+    },
+    leave(el, done) {
+      this.animar(el, done, true);
+    }
   }
 };
 </script>
@@ -34,27 +110,59 @@ export default {
   font-size: 1.5rem;
 }
 
-.fade-enter {
-
+.caixa {
+  height: 100px;
+  width: 300px;
+  margin: 30px auto;
+  background-color: aquamarine;
 }
 
-.fade-enter-active {
-
-}
-
-.fade-enter-to {
-
-}
-
-.fade-leave {
-
-}
-
-.fade-leave-active {
-
-}
-
+.fade-enter,
 .fade-leave-to {
+  opacity: 0;
+}
 
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(40px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(40px);
+  }
+}
+
+.slide-enter-active {
+  animation: slide-in 2s ease;
+  transition: opacity 2s;
+}
+
+.slide-leave-active {
+  position: absolute;
+  width: 100%;
+  animation: slide-out 2s;
+  transition: opacity 2s;
+}
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+
+.slide-move {
+  transition: transform 1s;
 }
 </style>
