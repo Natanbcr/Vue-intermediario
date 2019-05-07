13bde754f33fb1e9f02760c9876f41da0aa3c528
Natanbcr <natanbcr@primeinformatica.com.br>
2019-05-07 14:14:22 -0300

    axios completed

--- http/http-exercicios/src/plugins/axios.js
+++ http/http-exercicios/src/plugins/axios.js
@@ -1,10 +1,33 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://curso-vue-578d5.firebaseio.com/'
+//axios.defaults.baseURL = 'https://curso-vue-578d5.firebaseio.com/'
+// axios.defaults.headers.common['Authorization'] = 'abc123'
+// axios.defaults.headers.get['Accepts'] = 'application/json'
 
 Vue.use({
     install(Vue) {
-        Vue.prototype.$http = axios
+        // Vue.prototype.$http = axios
+        Vue.prototype.$http = axios.create({
+            baseURL: 'https://curso-vue-578d5.firebaseio.com/',
+            headers: {
+                "Authorization": "abc123"
+            }
+
+        })
+
+        Vue.prototype.$http.interceptors.request.use(config => {
+            console.log(config.method)
+            return config
+        }, error => Promise.reject(error))
+
+        Vue.prototype.$http.interceptors.response.use( res => {
+            // const array = []
+            // for(let chave in res.data) {
+            //     array.push({ id: chave, ...res.data[chave]})
+            // }
+            // res.data = array
+            return res
+        }, error => Promise.reject(error))
     }
 })
